7107757cb10e3304569a2ed0627671530b1e2cdb
atushimaku59-alt <atushimaku59@gmail.com>
2025-12-05 20:06:07 +0600

    depthfirstsearch.cpp

---
+++ depthfirstsearch.cpp
@@ -0,0 +1,52 @@
+```cpp
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int>* adj;
+    void DFSUtil(int v, vector<bool>& visited);
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void DFS(int s);
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w) {
+    adj[v].push_back(w);
+}
+
+void Graph::DFSUtil(int v, vector<bool>& visited) {
+    visited[v] = true;
+    cout << v << " ";
+
+    for (auto i : adj[v]) {
+        if (!visited[i])
+            DFSUtil(i, visited);
+    }
+}
+
+void Graph::DFS(int s) {
+    vector<bool> visited(V, false);
+    DFSUtil(s, visited);
+}
+
+int main() {
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    g.DFS(2);
+
+    return 0;
+}
